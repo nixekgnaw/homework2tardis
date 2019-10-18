f9e3129ed771fa6c725663a8567823b64b8606b7
Kfish <nixekgnaw@users.noreply.github.com>
2019-10-18 16:34:36 -0700

    Update mygemm.c
    边界处理，optimal

--- mygemm.c
+++ mygemm.c
@@ -94,25 +94,14 @@ void dgemm3(const double *A, const double *B, double *C, const int n)
                 register int tb = k * n + j;
                 register int ttb = tb + n;
                 register int tttb = ttb + n;
-                /*
-                * C00 = A00*B00 + A01*B10 + A02*B20
-                * C01 = A00*B01 + A01*B11 + A02*B21
-                * C02 = A00*B02 + A01*B12 + A02*B22
-                *
-                * C10 = A10*B00 + A11*B10 + A12*B20;
-                * C11 = A10*B01 + A11*B11 + A12*B21;
-                * C12 = A10*B02 + A11*B12 + A12*B22;
-                *
-                * C20 = A20*B00 + A21*B10 + A22*B20;
-                * C21 = A20*B01 + A21*B11 + A22*B21;
-                * C22 = A20*B02 + A21*B12 + A22*B22;
-                */
+
                 register double r1 = A[ta];     // a00
                 register double r2 = A[tta];    // a10
                 register double r3 = A[ttta];   // a20
                 register double r4 = B[tb];     // b00
                 register double r5 = B[tb + 1]; // b01
                 register double r6 = B[tb + 2]; // b02
+
                 c00 += r1 * r4;
                 c01 += r1 * r5;
                 c02 += r1 * r6;
@@ -173,7 +162,7 @@ void ijk(const double *A, const double *B, double *C, const int n)
     {
         for (j = 0; j < n; j++)
         {
-            register double sum = 0.0;
+            double sum = C[i * n + j];
             for (k = 0; k < n; k++)
                 sum += A[i * n + k] * B[k * n + j];
             C[i * n + j] = sum;
@@ -187,12 +176,12 @@ void bijk(const double *A, const double *B, double *C, const int n, const int b)
     for (i = 0; i < n; i += b)
         for (j = 0; j < n; j += b)
             for (k = 0; k < n; k += b)
-                for (i1 = i; i1 < i + b; i1++)
+                for (i1 = i; i1 < i + b && i1 < n; i1++)
                 {
-                    for (j1 = j; j1 < j + b; j1++)
+                    for (j1 = j; j1 < j + b && j1 < n; j1++)
                     {
                         register double sum = 0.0;
-                        for (k1 = k; k1 < k + b; k1++)
+                        for (k1 = k; k1 < k + b && k1 < n; k1++)
                             sum += A[i1 * n + k1] * B[k1 * n + j1];
                         C[i1 * n + j1] = sum;
                     }
@@ -221,12 +210,12 @@ void bjik(const double *A, const double *B, double *C, const int n, const int b)
     for (j = 0; j < n; j += b)
         for (i = 0; i < n; i += b)
             for (k = 0; k < n; k += b)
-                for (j1 = j; j1 < j + b; j1++)
+                for (j1 = j; j1 < j + b && j1 < n; j1++)
                 {
-                    for (i1 = i; i1 < i + b; i1++)
+                    for (i1 = i; i1 < i + b && i1 < n; i1++)
                     {
                         register double sum = 0.0;
-                        for (k1 = k; k1 < k + b; k1++)
+                        for (k1 = k; k1 < k + b && k1 < n; k1++)
                             sum += A[i1 * n + k1] * B[k1 * n + j1];
                         C[i1 * n + j1] = sum;
                     }
@@ -254,12 +243,12 @@ void bkij(const double *A, const double *B, double *C, const int n, const int b)
     for (k = 0; k < n; k += b)
         for (i = 0; i < n; i += b)
             for (j = 0; j < n; j += b)
-                for (k1 = k; k1 < k + b; k1++)
+                for (k1 = k; k1 < k + b && k1 < n; k1++)
                 {
-                    for (i1 = i; i1 < i + b; i1++)
+                    for (i1 = i; i1 < i + b && i1 < n; i1++)
                     {
                         register double r = A[i1 * n + k1];
-                        for (j1 = j; j1 < j + b; j1++)
+                        for (j1 = j; j1 < j + b && j1 < n; j1++)
                             C[i1 * n + j1] += r * B[k1 * n + j1];
                     }
                 }
@@ -286,12 +275,12 @@ void bikj(const double *A, const double *B, double *C, const int n, const int b)
     for (i = 0; i < n; i += b)
         for (k = 0; k < n; k += b)
             for (j = 0; j < n; j += b)
-                for (i1 = i; i1 < i + b; i1++)
+                for (i1 = i; i1 < i + b && i1 < n; i1++)
                 {
-                    for (k1 = k; k1 < k + b; k1++)
+                    for (k1 = k; k1 < k + b && k1 < n; k1++)
                     {
                         register double r = A[i1 * n + k1];
-                        for (j1 = j; j1 < j + b; j1++)
+                        for (j1 = j; j1 < j + b && j1 < n; j1++)
                             C[i1 * n + j1] += r * B[k1 * n + j1];
                     }
                 }
@@ -318,12 +307,12 @@ void bjki(const double *A, const double *B, double *C, const int n, const int b)
     for (j = 0; j < n; j += b)
         for (k = 0; k < n; k += b)
             for (i = 0; i < n; i += b)
-                for (j1 = j; j1 < j + b; j1++)
+                for (j1 = j; j1 < j + b && j1 < n; j1++)
                 {
-                    for (k1 = k; k1 < k + b; k1++)
+                    for (k1 = k; k1 < k + b && k1 < n; k1++)
                     {
                         register double r = B[k1 * n + j1];
-                        for (i1 = i; i1 < i + b; i1++)
+                        for (i1 = i; i1 < i + b && i1 < n; i1++)
                             C[i1 * n + j1] += A[i1 * n + k1] * r;
                     }
                 }
@@ -350,12 +339,12 @@ void bkji(const double *A, const double *B, double *C, const int n, const int b)
     for (k = 0; k < n; k += b)
         for (j = 0; j < n; j += b)
             for (i = 0; i < n; i += b)
-                for (k1 = k; k1 < k + b; k1++)
+                for (k1 = k; k1 < k + b && k1 < n; k1++)
                 {
-                    for (j1 = j; j1 < j + b; j1++)
+                    for (j1 = j; j1 < j + b && j1 < n; j1++)
                     {
                         register double r = B[k1 * n + j1];
-                        for (i1 = i; i1 < i + b; i1++)
+                        for (i1 = i; i1 < i + b && i1 < n; i1++)
                             C[i1 * n + j1] += A[i1 * n + k1] * r;
                     }
                 }
@@ -363,4 +352,93 @@ void bkji(const double *A, const double *B, double *C, const int n, const int b)
 
 void optimal(const double *A, const double *B, double *C, const int n, const int b)
 {
+    int i, j, k, i1, j1, k1;
+    for (i = 0; i < n; i += b)
+        for (j = 0; j < n; j += b)
+            for (k = 0; k < n; k += b)
+                for (i1 = i; i1 < i + b && i1 < n; i1 += 3)
+                {
+
+                    for (j1 = j; j1 < j + b && j1 < n; j1 += 3)
+                    {
+
+                        register int t = i1 * n + j1;
+                        register int tt = t + n;
+                        register int ttt = tt + n;
+                        register double c00 = C[t];
+                        register double c01 = C[t + 1];
+                        register double c02 = C[t + 2];
+                        register double c10 = C[tt];
+                        register double c11 = C[tt + 1];
+                        register double c12 = C[tt + 2];
+                        register double c20 = C[ttt];
+                        register double c21 = C[ttt + 1];
+                        register double c22 = C[ttt + 2];
+                        for (k1 = k; k1 < k + b && k1 < n; k1 += 3)
+                        {
+                            register int ta = i1 * n + k1;
+                            register int tta = ta + n;
+                            register int ttta = tta + n;
+                            register int tb = k1 * n + j1;
+                            register int ttb = tb + n;
+                            register int tttb = ttb + n;
+
+                            register double r1 = A[ta];     // a00
+                            register double r2 = A[tta];    // a10
+                            register double r3 = A[ttta];   // a20
+                            register double r4 = B[tb];     // b00
+                            register double r5 = B[tb + 1]; // b01
+                            register double r6 = B[tb + 2]; // b02
+
+                            c00 += r1 * r4;
+                            c01 += r1 * r5;
+                            c02 += r1 * r6;
+                            c10 += r2 * r4;
+                            c11 += r2 * r5;
+                            c12 += r2 * r6;
+                            c20 += r3 * r4;
+                            c21 += r3 * r5;
+                            c22 += r3 * r6;
+                            r1 = A[ta + 1];
+                            r2 = A[tta + 1];
+                            r3 = A[ttta + 1];
+                            r4 = B[ttb];
+                            r5 = B[ttb + 1];
+                            r6 = B[ttb + 2];
+                            c00 += r1 * r4;
+                            c01 += r1 * r5;
+                            c02 += r1 * r6;
+                            c10 += r2 * r4;
+                            c11 += r2 * r5;
+                            c12 += r2 * r6;
+                            c20 += r3 * r4;
+                            c21 += r3 * r5;
+                            c22 += r3 * r6;
+                            r1 = A[ta + 2];
+                            r2 = A[tta + 2];
+                            r3 = A[ttta + 2];
+                            r4 = B[tttb];
+                            r5 = B[tttb + 1];
+                            r6 = B[tttb + 2];
+                            c00 += r1 * r4;
+                            c01 += r1 * r5;
+                            c02 += r1 * r6;
+                            c10 += r2 * r4;
+                            c11 += r2 * r5;
+                            c12 += r2 * r6;
+                            c20 += r3 * r4;
+                            c21 += r3 * r5;
+                            c22 += r3 * r6;
+                        }
+                        C[t] = c00;
+                        C[t + 1] = c01;
+                        C[t + 2] = c02;
+                        C[tt] = c10;
+                        C[tt + 1] = c11;
+                        C[tt + 2] = c12;
+                        C[ttt] = c20;
+                        C[ttt + 1] = c21;
+                        C[ttt + 2] = c22;
+                    }
+                }
 }
