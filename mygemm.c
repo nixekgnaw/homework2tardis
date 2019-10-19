cf3686a9c3c127d63057fe29d43989a144787c5e
Kfish <nixekgnaw@users.noreply.github.com>
2019-10-18 17:13:39 -0700

    Update mygemm.c
    some 改动

--- mygemm.c
+++ mygemm.c
@@ -162,7 +162,7 @@ void ijk(const double *A, const double *B, double *C, const int n)
     {
         for (j = 0; j < n; j++)
         {
-            double sum = C[i * n + j];
+            register double sum = 0.0;
             for (k = 0; k < n; k++)
                 sum += A[i * n + k] * B[k * n + j];
             C[i * n + j] = sum;
@@ -256,17 +256,14 @@ void bkij(const double *A, const double *B, double *C, const int n, const int b)
 
 void ikj(const double *A, const double *B, double *C, const int n)
 {
-    /* ikj */
     int i, j, k;
     for (i = 0; i < n; i++)
-    {
         for (k = 0; k < n; k++)
         {
             register double r = A[i * n + k];
             for (j = 0; j < n; j++)
                 C[i * n + j] += r * B[k * n + j];
         }
-    }
 }
 
 void bikj(const double *A, const double *B, double *C, const int n, const int b)
@@ -276,14 +273,12 @@ void bikj(const double *A, const double *B, double *C, const int n, const int b)
         for (k = 0; k < n; k += b)
             for (j = 0; j < n; j += b)
                 for (i1 = i; i1 < i + b && i1 < n; i1++)
-                {
                     for (k1 = k; k1 < k + b && k1 < n; k1++)
                     {
                         register double r = A[i1 * n + k1];
                         for (j1 = j; j1 < j + b && j1 < n; j1++)
                             C[i1 * n + j1] += r * B[k1 * n + j1];
                     }
-                }
 }
 
 void jki(const double *A, const double *B, double *C, const int n)
