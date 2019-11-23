20cac061ef585d1f89339463ae1e52b15d36045c
Kfish <nixekgnaw@users.noreply.github.com>
2019-11-22 16:30:33 -0800

    Update sieve3.c

--- sieve3.c
+++ sieve3.c
@@ -127,35 +127,33 @@ int main(int argc, char *argv[]) {
     unsigned long int block_size = 1048576;
     // unsigned long int block_size = 2;
     unsigned long long int block_low_value = low_value;
-    unsigned long long int block_high_value = block_low_value + 2 * (block_size - 1);
+    unsigned long long int block_high_value;
+    unsigned long long int i;
+    for (i = block_low_value; i <= high_value; i + = 2 * block_size) {
+        block_high_value = block_low_value + 2 * (block_size - 1);
+        if (block_high_value > high_value)
+            block_high_value = high_value;
 
-    do
-    {
         index = 0;
         prime = 3;
         do {
             if (prime * prime > block_low_value)
-                first = (block_low_value-low_value) / 2 + (prime * prime - block_low_value) / 2;
+                first = (block_low_value - low_value) / 2 + (prime * prime - block_low_value) / 2;
             else {
-                if (!(block_low_value % prime)) first = (block_low_value-low_value) / 2;
+                if (!(block_low_value % prime))
+                    first = (block_low_value - low_value) / 2;
                 else if (block_low_value % prime % 2 == 0)
-                    first = (block_low_value-low_value) / 2 + prime - ((block_low_value % prime) / 2);
+                    first = (block_low_value - low_value) / 2 + prime - ((block_low_value % prime) / 2);
                 else
-                    first = (block_low_value-low_value) / 2 + (prime - (block_low_value % prime)) / 2;
+                    first = (block_low_value - low_value) / 2 + (prime - (block_low_value % prime)) / 2;
             }
             for (i = first; i < block_size; i += prime) {
                 marked[i] = 1;
             }
             while (local_prime_marked[++index]);
             prime = index * 2 + 3;
-        } while (prime * prime <= block_high_value);
-
-
-        block_low_value = block_high_value + 2;
-        block_high_value = block_low_value + 2 * (block_size - 1);
-        if (block_high_value > high_value)
-            block_high_value = high_value;
-    } while (block_low_value <= high_value);
+        } while (prime * prime <= n);
+    }
 
 
     count = 0;
