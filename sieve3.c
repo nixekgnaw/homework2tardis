d7eb84588e71923005d3ebe00b7adf4ac29559e8
Kfish <nixekgnaw@users.noreply.github.com>
2019-11-24 15:29:57 -0800

    Update sieve3.c

--- sieve3.c
+++ sieve3.c
@@ -120,67 +120,37 @@ int main(int argc, char *argv[]) {
     unsigned long long int block_low_value = low_value;
     unsigned long long int block_high_value = block_low_value + 2 * (block_size - 1);
 
-    do
-    {
-        /* code */
+    unsigned long int block_size = 1048576;
+    // unsigned long int block_size = 2;
+    unsigned long long int block_low_value;
+    unsigned long long int block_high_value;
+    for (block_low_value = low_value; block_low_value <= high_value; block_low_value += 2 * block_size) {
+        block_high_value = block_low_value + 2 * (block_size - 1);
+        if (block_high_value > high_value)
+            block_high_value = high_value;
+
         index = 0;
         prime = 3;
-        while(prime * prime <= block_high_value)
-        {
-            if(prime * prime >block_low_value)
-                first = (prime * prime - block_low_value)/2;
-            else
-            {
+        do {
+            if (prime * prime > block_low_value)
+                first = (prime * prime - block_low_value) / 2;
+            else {
                 if (!(block_low_value % prime))
                     first = 0;
-                else if(block_low_value % prime %2 ==0)
-                    first = prime - ((block_low_value % prime)/2);
+                else if (block_low_value % prime % 2 == 0)
+                    first =  prime - ((block_low_value % prime) / 2);
                 else
-                    first = (prime - (block_low_value % prime))/2;
-            }
-            for(i=first+(block_low_value - low_value)/2;i<=(block_high_value-low_value)/2;i+=prime)
-                marked[i]=1;
-            while(local_prime_marked[++index]==1);
-            prime = index *2 + 3;
-        }
-        block_low_value = block_high_value+2;
-        block_high_value = block_low_value + 2 *(block_size-1);
-        if(block_high_value > high_value)
-            block_high_value = high_value;
-    } while (block_low_value <= high_value);
+                    first =  (prime - (block_low_value % prime)) / 2;
 
-//
-//    unsigned long int block_size = 1048576;
-//    // unsigned long int block_size = 2;
-//    unsigned long long int block_low_value;
-//    unsigned long long int block_high_value;
-//    for (block_low_value = low_value; block_low_value <= high_value; block_low_value += 2 * block_size) {
-//        block_high_value = block_low_value + 2 * (block_size - 1);
-//        if (block_high_value > high_value)
-//            block_high_value = high_value;
-//
-//        index = 0;
-//        prime = 3;
-//        do {
-//            if (prime * prime > block_low_value)
-//                first = (prime * prime - block_low_value) / 2;
-//            else {
-//                if (!(block_low_value % prime))
-//                    first = 0;
-//                else if (block_low_value % prime % 2 == 0)
-//                    first =  prime - ((block_low_value % prime) / 2);
-//                else
-//                    first =  (prime - (block_low_value % prime)) / 2;
-//
-//            }
-//            first += (block_low_value - low_value) / 2;
-//            for (i = first; i < (block_high_value-low_value)/2; i += prime) {
-//                marked[i] = 1;
-//            }
-//            while (local_prime_marked[++index]);
-//            prime = index * 2 + 3;
-//        } while (prime * prime <= block_high_value);
-//    }
+            }
+            first += (block_low_value - low_value) / 2;
+            for (i = first; i < (block_high_value-low_value)/2; i += prime) {
+                marked[i] = 1;
+            }
+            while (local_prime_marked[++index]);
+            prime = index * 2 + 3;
+        } while (prime * prime <= block_high_value);
+    }
 
 
     count = 0;
