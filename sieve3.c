0ffa90eb036a5034a77f0a20e30fde914fac3e41
Kfish <nixekgnaw@users.noreply.github.com>
2019-11-24 16:14:33 -0800

    Update sieve3.c

--- sieve3.c
+++ sieve3.c
@@ -118,8 +118,7 @@ int main(int argc, char *argv[]) {
     unsigned long int block_size = 1048576;
     // unsigned long int block_size = 2;
     unsigned long long int block_low_value;
-    unsigned long long int block_high_value;
-    for (block_low_value = low_value; block_low_value <= high_value; block_low_value += 2 * block_size) {
+    unsigned long long int block_high_value;    for (block_low_value = low_value; block_low_value <= high_value; block_low_value += 2 * block_size) {
         block_high_value = block_low_value + 2 * (block_size - 1);
         if (block_high_value > high_value)
             block_high_value = high_value;
@@ -136,13 +135,10 @@ int main(int argc, char *argv[]) {
                     first = prime - ((block_low_value % prime) / 2);
                 else
                     first = (prime - (block_low_value % prime)) / 2;
-
             }
-            first += (block_low_value - low_value) / 2;
-            for (i = first; i < (block_high_value - low_value) / 2; i += prime) {
+            for (i = first + (block_low_value - low_value) / 2; i <= (block_high_value - low_value) / 2; i += prime)
                 marked[i] = 1;
-            }
-            while (local_prime_marked[++index]);
+            while (local_prime_marked[++index] == 1);
             prime = index * 2 + 3;
         }
     }
